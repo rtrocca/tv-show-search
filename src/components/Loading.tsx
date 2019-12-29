1f7752e47b18dd3fd58d8307225fa5373f4925a7
Riccardo Trocca <rtrocca@gmail.com>
2019-12-29 15:30:55 +0100

    Added loading screen and fixed alt img warnings

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import img from './Loading.gif';
 export const Loading: React.FC = () => {
     return (
-        <div>
-            Loading...
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", flexDirection: "column", marginTop: "2em"}}>
+            <img src={img} alt="Loading spinner"/>
+            <span style={{ 
+                marginTop: "1em"
+            }}>Loading...</span>
         </div>
     );
 }
