e0b2f38fc5584e62858803d48d4393437df4b641
Riccardo Trocca <rtrocca@gmail.com>
2019-12-29 15:37:57 +0100

    fix loading formatting

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import img from './Loading.gif';
 export const Loading: React.FC = () => {
     return (
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", flexDirection: "column", marginTop: "2em"}}>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                flexDirection: "column",
+                marginTop: "2em"
+            }}
+        >
             <img src={img} alt="Loading spinner"/>
             <span style={{ 
                 marginTop: "1em"
-            }}>Loading...</span>
+            }}
+            >
+                Loading...
+            </span>
         </div>
     );
 }
